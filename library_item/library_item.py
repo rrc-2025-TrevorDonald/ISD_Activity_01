b0f3a0f8eb0c347d374acc210b77e043bdedd27d
Trevor Donald <tdonald35@academic.rrc.ca>
2025-09-03 15:10:05 -0500

    Added accessors for the lirary item class and unit tested the __init__ and accessors

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -8,10 +8,24 @@ from genre.genre import Genre
 
 class LibraryItem():
     """
+    Description:
+        Class defined for performing functions of a Library Item.
     """
 
     def __init__(self, title:str, author:str, genre:Genre):
         """
+        Description:
+            Constructor for LibraryItem class.
+        
+        Args:
+            title (str): Title of the book.
+            author (str): Author of the book.
+            genre (Genre): Genre of the book.
+        
+        Raises:
+            ValueError(Title cannot be blank): When no name was entered
+            ValueError(Author cannot be blank): When no author was entered
+            ValueError(Invalid Genre): When the genre entered is invalid
         """
         if len(title) > 0:
             self.__title = title
@@ -27,3 +41,27 @@ class LibraryItem():
             self.__genre = genre
         else:
             raise ValueError("Invalid Genre.")
+
+    @property
+    def title (self) -> str:
+        """
+        Description:
+            Accessor for private attribute title.
+        """
+        return self.__title
+
+    @property
+    def author(self) -> str:
+        """
+        Description:
+            Accessor for private attribute author.
+        """
+        return self.__author
+
+    @property
+    def genre(self) -> Genre:
+        """
+        Description:
+            Accessor for private attribute genre.
+        """
+        return self.__genre
--- tests/test_library_item.py
+++ tests/test_library_item.py
@@ -9,3 +9,44 @@ __author__ = ""
 __version__ = ""
 
 import unittest
+from library_item.library_item import LibraryItem
+from genre.genre import Genre
+
+class TestClient(unittest.TestCase):
+    
+    def setUp(self):
+        self.libraryitem = LibraryItem("The_Lord_of_the_Rings",
+                                       "J.R.R. Tolkien", Genre.FANTASY)
+
+    def test_init_valid(self):
+        library_item = LibraryItem(
+            "The_Lord_of_the_Rings", "J.R.R. Tolkien", Genre.FANTASY)
+
+        self.assertEqual("The_Lord_of_the_Rings",
+                         library_item._LibraryItem__title)
+        self.assertEqual("J.R.R. Tolkien", library_item._LibraryItem__author)
+        self.assertEqual(Genre.FANTASY, library_item._LibraryItem__genre)
+
+    def test_init_title_valid(self):
+        expected = "Title cannot be blank."
+        with self.assertRaises(ValueError) as exception:
+            LibraryItem("", "J.R.R. Tolkien", Genre.FANTASY)
+
+        self.assertEqual(expected, str(exception.exception))
+
+    def test_init_author_valid(self):
+        with self.assertRaises(ValueError):
+            LibraryItem("The_Lord_of_the_Rings", "", Genre.FANTASY)
+
+    def test_init_genre_valid(self):
+        with self.assertRaises(ValueError):
+            LibraryItem("The_Lord_of_the_Rings", "J.R.R. Tolkien", "Invalid")
+
+    def test_title_accessor(self):
+        self.assertEqual("The_Lord_of_the_Rings", self.libraryitem.title)
+
+    def test_author_accessor(self):
+        self.assertEqual("J.R.R. Tolkien", self.libraryitem.author)
+
+    def test_genre_accessor(self):
+        self.assertEqual(Genre.FANTASY, self.libraryitem.genre)
