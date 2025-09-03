fc8a3a7522d276dc1804dea672044b3ebc3aa1da
Trevor Donald <tdonald35@academic.rrc.ca>
2025-09-03 18:49:10 -0500

    Added item id and is valid, made accessors and unit tested confirming they all work

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -12,21 +12,33 @@ class LibraryItem():
         Class defined for performing functions of a Library Item.
     """
 
-    def __init__(self, title:str, author:str, genre:Genre):
+    def __init__(self, item_id:int, title:str, author:str, genre:Genre,
+                 is_borrowed:bool):
         """
         Description:
             Constructor for LibraryItem class.
         
         Args:
+            item_id (int): Id number if the book.
             title (str): Title of the book.
             author (str): Author of the book.
             genre (Genre): Genre of the book.
+            is_borrowed (boolean): Is the book taken out.
         
         Raises:
-            ValueError(Title cannot be blank): When no name was entered
-            ValueError(Author cannot be blank): When no author was entered
-            ValueError(Invalid Genre): When the genre entered is invalid
+            ValueError(Item id must be numeric.) When the value entered is not
+            numeric.
+            ValueError(Title cannot be blank.): When no name was entered.
+            ValueError(Author cannot be blank.): When no author was entered.
+            ValueError(Invalid Genre.): When the genre entered is invalid.
+            ValueError(Is Borrowed must be a boolean value.): When the value
+            entered in not a boolean.  
         """
+        if isinstance(item_id, int):
+            self.__item_id = item_id
+        else:
+            raise ValueError("Item id must be numeric.")
+
         if len(title) > 0:
             self.__title = title
         else:
@@ -41,6 +53,19 @@ class LibraryItem():
             self.__genre = genre
         else:
             raise ValueError("Invalid Genre.")
+
+        if isinstance(is_borrowed, bool):
+            self.__is_borrowed = is_borrowed
+        else:
+            raise ValueError("Is Borrowed must be a boolean value.")
+
+    @property
+    def item_id(self) -> int:
+        """
+        Description:
+            Accessor for private attribute item_id.
+        """
+        return self.__item_id
 
     @property
     def title (self) -> str:
@@ -65,3 +90,11 @@ class LibraryItem():
             Accessor for private attribute genre.
         """
         return self.__genre
+
+    @property
+    def is_borrowed(self):
+        """
+        Description:
+            Accessor for private attribute is_borrowed.
+        """
+        return self.__is_borrowed
--- tests/test_library_item.py
+++ tests/test_library_item.py
@@ -13,34 +13,59 @@ from library_item.library_item import LibraryItem
 from genre.genre import Genre
 
 class TestClient(unittest.TestCase):
+    """
+    Description:
+        Class defined for testing all funtions of the LibraryItem class.
+    """
 
     def setUp(self):
-        self.libraryitem = LibraryItem("The_Lord_of_the_Rings",
-                                       "J.R.R. Tolkien", Genre.FANTASY)
+        """
+        Description:
+            Setup function for simplifying tests
+        """
+        self.libraryitem = LibraryItem(9,"The_Lord_of_the_Rings",
+                                       "J.R.R. Tolkien", Genre.FANTASY, True)
 
     def test_init_valid(self):
         library_item = LibraryItem(
-            "The_Lord_of_the_Rings", "J.R.R. Tolkien", Genre.FANTASY)
+                9, "The_Lord_of_the_Rings", "J.R.R. Tolkien", Genre.FANTASY,
+                True)
 
+        self.assertEqual(9, library_item._LibraryItem__item_id)
         self.assertEqual("The_Lord_of_the_Rings",
                          library_item._LibraryItem__title)
         self.assertEqual("J.R.R. Tolkien", library_item._LibraryItem__author)
         self.assertEqual(Genre.FANTASY, library_item._LibraryItem__genre)
+        self.assertEqual(True, library_item._LibraryItem__is_borrowed)
+
+    def test_init_item_id_valid(self):
+        with self.assertRaises(ValueError):
+            LibraryItem("9", "The_Lord_of_the_Rings",
+                        "J.R.R. Tolkien", "Invalid", True)
 
     def test_init_title_valid(self):
         expected = "Title cannot be blank."
         with self.assertRaises(ValueError) as exception:
-            LibraryItem("", "J.R.R. Tolkien", Genre.FANTASY)
+            LibraryItem(9, "", "J.R.R. Tolkien", Genre.FANTASY, True)
 
         self.assertEqual(expected, str(exception.exception))
 
     def test_init_author_valid(self):
         with self.assertRaises(ValueError):
-            LibraryItem("The_Lord_of_the_Rings", "", Genre.FANTASY)
+            LibraryItem(9, "The_Lord_of_the_Rings", "", Genre.FANTASY, True)
 
     def test_init_genre_valid(self):
         with self.assertRaises(ValueError):
-            LibraryItem("The_Lord_of_the_Rings", "J.R.R. Tolkien", "Invalid")
+            LibraryItem(9, "The_Lord_of_the_Rings",
+                        "J.R.R. Tolkien", "Invalid", True)
+
+    def test_init_is_borrowed_valid(self):
+        with self.assertRaises(ValueError):
+            LibraryItem(9, "The_Lord_of_the_Rings",
+                        "J.R.R. Tolkien", "Invalid", "Invalid")
+
+    def test_item_id_accessor(self):
+        self.assertEqual(9, self.libraryitem.item_id)
 
     def test_title_accessor(self):
         self.assertEqual("The_Lord_of_the_Rings", self.libraryitem.title)
@@ -50,3 +75,6 @@ class TestClient(unittest.TestCase):
 
     def test_genre_accessor(self):
         self.assertEqual(Genre.FANTASY, self.libraryitem.genre)
+
+    def test_is_borrowed_accessor(self):
+        self.assertEqual(True, self.libraryitem.is_borrowed)
